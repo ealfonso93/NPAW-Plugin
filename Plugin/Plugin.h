6f17e443758fc91bfcd26baee6fc49e44d96005f
Sergio Sanchez Del Rio <sergiosanchezdelrio@iMac-de-Sergio.local>
2017-05-21 17:37:28 +0200

    Initial Commit

---
+++ Plugin/Plugin.h
@@ -0,0 +1,13 @@
+//
+//  Plugin.h
+//  Plugin
+//
+//  Created by Sergio Sanchez Del Rio on 21/5/17.
+//  Copyright © 2017 Sergio Sanchez Del Rio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Plugin : NSObject
+
+@end
---
+++ Plugin/Plugin.m
@@ -0,0 +1,13 @@
+//
+//  Plugin.m
+//  Plugin
+//
+//  Created by Sergio Sanchez Del Rio on 21/5/17.
+//  Copyright © 2017 Sergio Sanchez Del Rio. All rights reserved.
+//
+
+#import "Plugin.h"
+
+@implementation Plugin
+
+@end
